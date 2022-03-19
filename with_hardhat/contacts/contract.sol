84bd199bdcc846a5715a0f00070a7df9f70c3b7f
faaaaasih <fasihq38@gmail.com>
2022-03-19 15:53:00 +0000

    implemented test and contract

---
+++ with_hardhat/contacts/contract.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+contract newContract {
+
+    string private name;
+    address public s_address;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+
+    function setaddress() public {
+        s_address = msg.sender;
+    }
+
+}
