ca9c207e46bd401fe68b8b4d876be0623a168d11
faaaaasih <fasihq38@gmail.com>
2022-03-19 15:03:32 +0000

    install provider

--- contract.sol
+++ contract.sol
@@ -4,12 +4,20 @@ pragma solidity ^0.8.12;
 contract Test {
 
     string public myname;
-    address public sender_address;
-    
+    address[] public address_array;
 
-constructor () {
+
+function setName(string memory message) public {
+    myname = message;
+}
+
+function getAddress() public view returns (address){
+    return msg.sender;
 
 }
 
+function addaddress() public {
+    address_array.push(msg.sender);
+}
 
 }
