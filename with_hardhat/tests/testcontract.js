ec65fc8a9a378d355848c2a21109fd3ed67940a6
faaaaasih <fasihq38@gmail.com>
2022-03-21 19:41:53 +0000

    compiled second version of contract

---
+++ with_hardhat/tests/testcontract.js
@@ -0,0 +1,22 @@
+const {expect} = require('chai')
+
+
+describe ("MyContractTest", () => {
+    it("should return its name", async() => {
+        const contract_c = await ethers.getContractFactory("contract")
+        const contract_d = await contract_c.deploy("My Contract")
+
+        await contract_d.deployed()
+
+        expect(await contract_d.)
+
+
+
+    })
+
+
+
+
+
+
+})
