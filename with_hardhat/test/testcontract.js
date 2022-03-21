efc00e330552650edb78af30dd6924f307fe9642
faaaaasih <fasihq38@gmail.com>
2022-03-21 20:07:16 +0000

    changes with test

--- with_hardhat/test/testcontract.js
+++ with_hardhat/test/testcontract.js
@@ -3,8 +3,11 @@ const {expect} = require('chai')
 
 describe ("MyContractTest", () => {
     it("should return its name", async() => {
-        const contract_c = await ethers.getContractFactory("contract")
+        const contract_c = await ethers.getContractFactory("newContract")
         const contract_d = await contract_c.deploy("My Contract")
+
+        const account_owner = await ethers.getSigners()
+
 
         await contract_d.deployed()
 
